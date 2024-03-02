4d5310e3eba35428b5048c27b6b160897d0983a1
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-03-02 23:42:47 +0900

    평균을 내서 비교하기

--- lib/24_03_02/24_03_02_2.dart
+++ lib/24_03_02/24_03_02_2.dart
@@ -1,2 +1,16 @@
 //Q. How good are you really?
 
+void main () {
+  final result = betterThanAverage([80, 70, 70, 65], 97);
+  print(result);
+}
+
+bool betterThanAverage(List<int> classPoints, int yourPoints) {
+  int sum = classPoints.reduce((value, element) => value + element);
+  double average = sum/classPoints.length;
+  if(yourPoints > average) {
+    return true;
+  }
+  return false;
+
+}
