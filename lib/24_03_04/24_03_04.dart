2946613a67627554af00282beddf76e3c4489c4d
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-03-05 00:04:34 +0900

    한줄로 수정

--- lib/24_03_04/24_03_04.dart
+++ lib/24_03_04/24_03_04.dart
@@ -1,11 +1,14 @@
 //Q. Difference of Volumes of Cuboids
 
-void main () {
+void main() {}
 
-}
+// int findDifference(List<int> a, List<int> b) {
+// final aVolumn = a.reduce((v, e) => v*e);
+// final bVolumn = b.reduce((v, e) => v*e);
+// return (aVolumn - bVolumn).abs();
+// }
 
+//한줄
 int findDifference(List<int> a, List<int> b) {
-final aVolumn = a.reduce((v, e) => v*e);
-final bVolumn = b.reduce((v, e) => v*e);
-return aVolumn - bVolumn;
+  return (a.reduce((v, e) => v * e) - b.reduce((v, e) => v * e)).abs();
 }
