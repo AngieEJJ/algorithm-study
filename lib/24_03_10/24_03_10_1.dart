923fcdc9c4e1d43f2b4a029880bfad2249222780
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-03-10 20:13:27 +0900

    Reverse List Order

---
+++ lib/24_03_10/24_03_10_1.dart
@@ -0,0 +1,5 @@
+//Q. Reverse List Order
+
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+List<int> reverseList(List<int> list) => list.reversed.toList();
